8d7b6f3bcb938bea45c3af7616e6727813b10133
Miron Bury <miron.bury@gmail.com>
2020-10-06 02:23:36 +0100

    make obstacle movable

---
+++ Assets/Scripts/Movable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movable : MonoBehaviour
+{
+    private bool movable = false;
+
+    Rigidbody m_Rigidbody;
+
+    private void Start() {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_Rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+    }
+
+	private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Strong") {
+            m_Rigidbody.constraints = RigidbodyConstraints.None;
+        }
+    }
+    private void OnTriggerExit(Collider other) {
+        m_Rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+    }
+}
