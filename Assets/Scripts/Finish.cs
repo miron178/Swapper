c47fa8e6824680bdf604c192c4a3e09331e59dc1
Miron Bury <miron.bury@gmail.com>
2020-10-07 07:26:58 +0100

    added scenes and finish git ignore failure

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finish : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Swappable") || other.gameObject.layer == LayerMask.NameToLayer("Slime")) {
+            int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+    }
+}
