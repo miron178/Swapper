0b9a583bd5582d8690e19fa3e7b57749fe57efc0
Tomek Bury <tomek@broadcom.com>
2020-11-10 21:02:51 +0000

    add comment
    just for testing

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// pull request test
+
 public class Finish : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other) {
