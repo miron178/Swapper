2d3bd2f1b9941b0ae4b103d4a1d9bacfd9571c55
Miron Bury <miron.bury@gmail.com>
2020-11-10 21:15:07 +0000

    being more specific

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-// pull request test
+// pull request test - more specific
 
 public class Finish : MonoBehaviour
 {
