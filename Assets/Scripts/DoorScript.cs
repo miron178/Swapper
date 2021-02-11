9381f9a8535ded39a9b0147dacef4c26921554e3
Ffiffi27 <55792901+Ffiffi27@users.noreply.github.com>
2021-02-11 18:38:46 +0000

    Faster Slime + Script for door (not done yet)

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public GameObject doorObject;
+
+    private bool DoorOpen;
+    private bool buttonPressed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag != "Player")
+        {
+            DoorOpen = !DoorOpen;
+            doorObject.GetComponent<Animator>().SetBool("DoorOpen", DoorOpen);
+        }
+    }
+}
