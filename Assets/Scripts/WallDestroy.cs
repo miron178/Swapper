a75cf1482c56ccdf10b0e267f7f03c89c73384a9
Ffiffi27 <55792901+Ffiffi27@users.noreply.github.com>
2021-02-26 15:18:32 +0000

    Wall Destroy Script...
    + invisible button to close the door.

---
+++ Assets/Scripts/WallDestroy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallDestroy : MonoBehaviour
+{
+    public GameObject destroyedVersion;
+
+    void OnTriggerEnter()
+    {
+        Instantiate(destroyedVersion, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
