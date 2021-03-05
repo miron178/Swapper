ed2e6019097a8b1e584c77bf1ae33f82dfb4e1df
Ffiffi27 <55792901+Ffiffi27@users.noreply.github.com>
2021-03-05 18:55:41 +0000

    Swappable Code fixed :)
    Also has the start of the Health bar and player health. Changed "Player" script to "SwapScript" since there is a separate movement script.

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 10;
+    public int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        //if ()
+        //{
+        //
+        //}
+    }
+}
--- Assets/Scripts/SwapScript.cs
+++ Assets/Scripts/SwapScript.cs
@@ -3,23 +3,12 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class SwapScript : MonoBehaviour
 {
-	//privates for player control
-	[SerializeField]
-	private float moveForce = 1000f;
 	private Rigidbody rb;
-	[SerializeField]
-	private float maxVelocity = 1f;
-	[SerializeField]
-	private float jumpForce = 10f;
-	[SerializeField]
-	private GameObject jumpCheck;
-	[SerializeField]
-	private bool canJump = false;
-	[SerializeField]
-	private float rotationSpeed = 5f;
-	
+
+	public Movement movementScript;
+
 	//privates for active
 	[SerializeField]
 	private bool active = false;
@@ -50,7 +39,6 @@ public class Player : MonoBehaviour
 
 	void Start() {
 		rb = GetComponent<Rigidbody>(); //used later to make code more readable
-
 		//layer dependancies
 		groundLayer = LayerMask.NameToLayer("Ground");
 		testLayer = LayerMask.NameToLayer("Test");
@@ -58,6 +46,8 @@ public class Player : MonoBehaviour
 		wallLayer = LayerMask.NameToLayer("Wall");
 
 		meshRenderer = this.GetComponentsInChildren<MeshRenderer>();
+
+		movementScript = GetComponent<Movement>();
 	}
 
 	void FixedUpdate() {
@@ -70,30 +60,11 @@ public class Player : MonoBehaviour
 		}
 
 		if (active) {
-			float horizontal = Input.GetAxis("Horizontal"); //movment
-			float vertical = Input.GetAxis("Vertical"); //movment
-
-			float jump = jumpForce * Input.GetAxis("Jump"); //jump
-
 			float interact = Input.GetAxis("Interact"); //swap
 
 			float spawn = Input.GetAxis("Spawn"); //spawn
 
-			if (horizontal != 0f || active && vertical != 0f) {
-				Rotate(horizontal, vertical);
-				Move(horizontal, vertical);
-			}
-
-			//Jump
-			bool grounded = Physics.Linecast(transform.position, jumpCheck.transform.position, (1 << groundLayer)); //checks if player is grounded
-			/*~ == one's compliment*/
-			/*1 << groundLayer == 1<<8*/
-			/*1<<8 == 11111111 11111111 11111110 11111111*/
-			if (grounded && canJump) {
-				//Debug.Log("why");
-				rb.AddRelativeForce(Vector3.up * jump * Time.fixedDeltaTime);
-			}
-
+			movementScript.enabled = true;
 			if (interact > 0 && objectInRange && !interactRelease) {
 				Swap();
 				interactRelease = true;
@@ -119,7 +90,7 @@ public class Player : MonoBehaviour
 		}
 	}
 	private void Swap() {
-		Player playerInRange = objectInRange.GetComponent<Player>();
+		SwapScript playerInRange = objectInRange.GetComponent<SwapScript>();
 		playerInRange.active = true;
 
 		if (this.gameObject.layer != LayerMask.NameToLayer("Slime")) {
@@ -133,29 +104,5 @@ public class Player : MonoBehaviour
 	private void SpawnSlime() {
 		GameObject slime = Instantiate(slimePrefab, spawnPoint.position, spawnPoint.rotation);
 		this.active = false;
-	}
-
-	void LateUpdate() {
-		LimitVelocity();
-	}
-
-	private void Rotate(float horizontal, float vertical) {
-		// look towards the move direction
-		Quaternion lookHere = new Quaternion();
-		lookHere.SetLookRotation(new Vector3(horizontal, 0, vertical));
-
-		// smooth rotation
-		transform.rotation = Quaternion.Slerp(transform.rotation, lookHere, Time.deltaTime * rotationSpeed);
-	}
-
-	private void Move(float horizontal, float vertical) {
-		float force = Mathf.Max(Mathf.Abs(horizontal), Mathf.Abs(vertical));
-		rb.AddRelativeForce(Vector3.forward * force * moveForce * Time.fixedDeltaTime);
-	}
-
-	private void LimitVelocity() {
-		if (rb.velocity.magnitude > maxVelocity) {
-			rb.velocity = rb.velocity.normalized * maxVelocity;
-		}
 	}
 }
