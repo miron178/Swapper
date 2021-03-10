528a01f2e469d0adeaa1ccf0bf72ceab0ecb7099
Miron Bury <miron.bury@gmail.com>
2021-03-10 19:06:19 +0000

    temp wall destroy
    add rb to wall (kinematic)

--- Assets/Scripts/WallDestroy.cs
+++ Assets/Scripts/WallDestroy.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class WallDestroy : MonoBehaviour
 {
-    public GameObject destroyedVersion;
+    //public GameObject destroyedVersion;
 
+#if true
+    void OnCollisionEnter(Collision other)
+	{
+        if (other.gameObject.tag == "Strong")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+#else
     void OnTriggerEnter()
     {
         Instantiate(destroyedVersion, transform.position, transform.rotation);
         Destroy(gameObject);
     }
+#endif
 }
