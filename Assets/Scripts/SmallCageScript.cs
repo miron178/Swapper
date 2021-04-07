0b7c33ce0ee4560b743f07fb05a7d5d329d7faa4
Ffiffi27 <55792901+Ffiffi27@users.noreply.github.com>
2021-04-07 18:54:45 +0100

    Small Cage in Level 3 is animated :)

---
+++ Assets/Scripts/SmallCageScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallCageScript : MonoBehaviour
+{
+    public GameObject smallCageObject;
+
+    private bool CageOpen;
+    private bool buttonPressed;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            CageOpen = !CageOpen;
+            smallCageObject.GetComponent<Animator>().SetBool("CageOpen", CageOpen);
+        }
+    }
+}
