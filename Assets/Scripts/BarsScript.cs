d3a2a9576b763340d11b2090535f06e0275b4f58
Ffiffi27 <55792901+Ffiffi27@users.noreply.github.com>
2021-04-14 20:18:55 +0100

    Bars go up when button is pressed :)

---
+++ Assets/Scripts/BarsScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarsScript : MonoBehaviour
+{
+    public GameObject barsObject;
+
+    private bool BarsOpen;
+    private bool buttonPressed;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            BarsOpen = !BarsOpen;
+            barsObject.GetComponent<Animator>().SetBool("BarsOpen", BarsOpen);
+        }
+    }
+}
