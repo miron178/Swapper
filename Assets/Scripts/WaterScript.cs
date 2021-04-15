bb0299cde224e7860311a1a3d0da96fe9bb6b1a0
Ffiffi27 <55792901+Ffiffi27@users.noreply.github.com>
2021-04-15 13:35:19 +0100

    Slimes die when touches the water :)

---
+++ Assets/Scripts/WaterScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WaterScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else if(other.gameObject.tag == "Small")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
