646f20de938f7309f676309461a211bfa735fa1e
Ffiffi27 <55792901+Ffiffi27@users.noreply.github.com>
2021-04-17 16:43:44 +0100

    ESCAPE!

---
+++ Assets/Scripts/EcsapeScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class EcsapeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Esc") != 0)
+        {
+            Debug.Log("Main Menu");
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
